d0ea7336bbdb8e251447f01df7d102a719bc5cdb
Anthony Wong <awmwong@gmail.com>
2013-06-18 23:26:25 -0400

    Added Settings ViewController

---
+++ covrme/Source/CMSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMSettingsViewController.h
+//  covrme
+//
+//  Created by Anthony Wong on 2013-06-18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMSettingsViewController : UIViewController
+
+@end
---
+++ covrme/Source/CMSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  CMSettingsViewController.m
+//  covrme
+//
+//  Created by Anthony Wong on 2013-06-18.
+//
+//
+
+#import "CMSettingsViewController.h"
+
+@interface CMSettingsViewController ()
+
+@end
+
+@implementation CMSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
