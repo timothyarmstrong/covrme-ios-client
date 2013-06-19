6495996ea06d79e55bb939c0011aee42607671ed
Anthony Wong <awmwong@gmail.com>
2013-06-18 23:25:55 -0400

    Added History List Controllers

---
+++ covrme/Source/CMHistoryListViewController.h
@@ -0,0 +1,14 @@
+//
+//  CMHistoryListViewController.h
+//  covrme
+//
+//  Created by Anthony Wong on 2013-05-18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMHistoryListViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
---
+++ covrme/Source/CMHistoryListViewController.m
@@ -0,0 +1,38 @@
+//
+//  CMHistoryListViewController.m
+//  covrme
+//
+//  Created by Anthony Wong on 2013-05-18.
+//
+//
+
+#import "CMHistoryListViewController.h"
+
+@interface CMHistoryListViewController ()
+
+@end
+
+@implementation CMHistoryListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
