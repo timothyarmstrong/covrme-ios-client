ffa7eb8a89cebd97b890befbcc35b84d54ba1de5
Anthony Wong <awmwong@gmail.com>
2013-06-18 23:40:55 -0400

    FrontDoor VC

---
+++ covrme/Source/CMFrontDoorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMFrontDoorViewController.h
+//  covrme
+//
+//  Created by Anthony Wong on 2013-06-18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMFrontDoorViewController : UIViewController
+
+@end
---
+++ covrme/Source/CMFrontDoorViewController.m
@@ -0,0 +1,38 @@
+//
+//  CMFrontDoorViewController.m
+//  covrme
+//
+//  Created by Anthony Wong on 2013-06-18.
+//
+//
+
+#import "CMFrontDoorViewController.h"
+
+@interface CMFrontDoorViewController ()
+
+@end
+
+@implementation CMFrontDoorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
